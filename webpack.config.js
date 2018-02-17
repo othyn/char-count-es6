e7300070f86481de28198a229db24adbeebfcce9
Ben <github@othyn.com>
2018-02-17 20:05:29 +0000

    Firm up webpack config
    Think I've got everything I need for now

--- webpack.config.js
+++ webpack.config.js
@@ -3,51 +3,79 @@ const path = require('path');
 // Imports
 
 const UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
+const HotModuleReplacementPlugin = webpack.HotModuleReplacementPlugin;
 // Webpack plugins
 
-let plugins = [],
-    outputFile;
-// Store build options
+module.exports = env => {
 
-if (env === 'production') {
-    plugins.push(new UglifyJsPlugin({ minimize: true }));
-    outputFile = 'char-count.min.js';
-} else {
-    outputFile = 'char-count.js';
-}
-// Build environment options
+    // As I need to use env, webpack needs to be defined as a function
 
-module.exports = {
-    entry: path.resolve('./src/char-count.js'),
-    devtool: 'source-map',
-    output: {
-        path: path.resolve('./dist'),
-        filename: outputFile,
-        library: 'char-count',
-        libraryTarget: 'umd',
-        umdNamedDefine: true
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/
-            },
-            {
-                test: /\.js$/,
-                loader: 'eslint-loader',
-                exclude: /node_modules/
-            }
-        ]
-    },
-    resolve: {
-        modules: [
-            path.resolve('./node_modules'),
-            path.resolve('./src')
-        ],
-        extensions: ['.json', '.js']
-    },
-    plugins: plugins
+    let plugins = [],
+        outputFile = ''
+        outputPath = '';
+    // Store build options
+
+    if (env === 'prod') {
+
+        plugins.push(new UglifyJsPlugin({ minimize: true }));
+        // Minify on prod build
+
+        outputFile = 'char-count.min.js';
+        outputPath = path.resolve('./dist');
+        // Prod build location/file
+
+    } else {
+
+        plugins.push(new HotModuleReplacementPlugin());
+        // Hot module reloading in dev server
+
+        outputFile = 'char-count.js';
+        outputPath = path.resolve('./dev');
+        // Dev build location/file
+    }
+
+    return {
+        entry: path.resolve('./src/char-count.js'),
+        devtool: 'source-map',
+        output: {
+            path: path.resolve('./dist'),
+            filename: outputFile,
+            library: 'char-count',
+            libraryTarget: 'umd',
+            umdNamedDefine: true
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    loader: 'babel-loader',
+                    exclude: /node_modules/
+                },
+                {
+                    test: /\.js$/,
+                    loader: 'eslint-loader',
+                    exclude: /node_modules/
+                }
+            ]
+        },
+        resolve: {
+            modules: [
+                path.resolve('./node_modules'),
+                path.resolve('./src')
+            ],
+            extensions: ['.json', '.js']
+        },
+        plugins: plugins,
+        devServer: {
+            publicPath: '/',
+            port: 8080,
+            contentBase: path.resolve('./dev'),
+            host: 'localhost',
+            historyApiFallback: true,
+            noInfo: false,
+            stats: 'minimal',
+            hot: true
+        }
+    }
 };
 // Webpack Config
