8358116bf4ad2380ed2fe3a7659711004d7d3abd
Ben <github@othyn.com>
2018-02-24 16:44:16 +0000

    Example callback implementation

--- src/example.app.js
+++ src/example.app.js
@@ -2,5 +2,35 @@ import CharCount from './char-count';
 // Hmm, maybe need to rename the lib file to CharCount
 // to make this a little less confusing...
 
-new CharCount();
+new CharCount({
+
+    onFieldEmpty: (field) => {
+        console.log('onFieldEmpty');
+    },
+
+    onFieldWarningReached: (field, count) => {
+        console.log('onFieldWarningReached');
+    },
+
+    onFieldWarningWithdrawn: (field, count) => {
+        console.log('onFieldWarningWithdrawn');
+    },
+
+    onFieldDangerReached: (field, count) => {
+        console.log('onFieldDangerReached');
+    },
+
+    onFieldDangerWithdrawn: (field, count) => {
+        console.log('onFieldDangerWithdrawn');
+    },
+
+    onFieldLimitReached: (field, count) => {
+        console.log('onFieldLimitReached');
+    },
+
+    onFieldLimitWithdrawn: (field, count) => {
+        console.log('onFieldLimitWithdrawn');
+    },
+
+});
 // Setup a new instance
