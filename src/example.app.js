c20d0e8bdd28da3bb2c24c42797a02a95f6d2766
Ben <github@othyn.com>
2018-02-25 02:25:35 +0000

    Provide bootstrap form example
    Damn, they removed warning states in the final realease of 4.0....

--- src/example.app.js
+++ src/example.app.js
@@ -9,23 +9,43 @@ import CharCount from './char-count';
 new CharCount({
 
     onFieldEmpty: (field) => {
+
         console.log('onFieldEmpty');
+
+        field.className = 'form-control cc-field';
+        // Reset Bootstrap form state
     },
 
     onFieldFine: (field) => {
+
         console.log('onFieldFine');
+
+        field.className = 'form-control cc-field is-valid';
+        // Reset Bootstrap form state
     },
 
     onFieldWarning: (field, count) => {
+
         console.log('onFieldWarning');
+
+        field.className = 'form-control cc-field';
+        // Set field to Bootstrap warning state
     },
 
     onFieldDanger: (field, count) => {
+
         console.log('onFieldDanger');
+
+        field.className = 'form-control cc-field is-invalid';
+        // Set field to Bootstrap danger state
     },
 
     onFieldLimit: (field, count) => {
+
         console.log('onFieldLimit');
+
+        field.className = 'form-control cc-field is-invalid';
+        // Set field to Bootstrap danger state
     }
 
 });
