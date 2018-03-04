be55f572fc253fa4cfcedba715f6e2ef8824837d
Ben <github@othyn.com>
2018-03-04 23:07:06 +0000

    Rename project for NPM

--- webpack.config.js
+++ webpack.config.js
@@ -82,7 +82,7 @@ module.exports = env => {
         output: {
             path: outputPath,
             filename: outputFile,
-            library: 'CharCount',
+            library: 'char-count-es6',
             libraryTarget: 'umd',
             umdNamedDefine: true
         },
